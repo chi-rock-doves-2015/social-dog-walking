e5f3a578c5e0e9442bbcd96e76218b70437a80e1
Christopher Bohl & joshzich <pair+cbohl.joshzich@devbootcamp.com>
2015-05-07 20:45:24 -0500

    testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -101,17 +101,17 @@ RSpec.configure do |config|
 
   config.infer_base_class_for_anonymous_controllers = false
 
-  config.order = "random"
+  # config.order = "random"
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-  end
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :truncation
+  # end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
 end
