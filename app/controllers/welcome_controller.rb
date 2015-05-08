98b6bb72e5b141f92d10182cb2d1934fa17a963a
Erik Stockmeier <erik.d.stock@gmail.com>
2015-05-07 20:22:01 -0500

    set up maps api and map-sandbox route

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
-  def index
+  def map_sandbox
+  	unless Rails.env.development?
+  		flash[:alert] = "Production area only"
+  		redirect_to '/'
+  	end
+  		flash[:alert] = "Welcome to Warp Zone"
   end
 end
