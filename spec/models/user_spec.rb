9430894f487eca29ff29d70f97fe72db1ab2f5e2
Christopher Bohl & joshzich <pair+cbohl.joshzich@devbootcamp.com>
2015-05-08 07:47:02 -0500

    passing passing travis

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let!(:gaber) {User.create!(pet_name: "Gaber", breed: "pomeranian", email: 'gaber@gaber.com', password: 'password')}
-  pending "add some examples to (or delete) #{__FILE__}"
   it 'is a user' do
     expect(gaber).to be_instance_of(User)
   end
