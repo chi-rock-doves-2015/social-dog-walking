6e3aed068fbf07fd8d809ec608e238c2e8129745
Christopher Bohl & joshzich <pair+cbohl.joshzich@devbootcamp.com>
2015-05-07 20:57:59 -0500

    testing 123

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,21 +97,21 @@ RSpec.configure do |config|
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   config.infer_base_class_for_anonymous_controllers = false
 
-  # config.order = "random"
+  config.order = "random"
 
-  # config.before(:suite) do
-  #   DatabaseCleaner.strategy = :truncation
-  # end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
 
-  # config.before(:each) do
-  #   DatabaseCleaner.start
-  # end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
-  # config.after(:each) do
-  #   DatabaseCleaner.clean
-  # end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
