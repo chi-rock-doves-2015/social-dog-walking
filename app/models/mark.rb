5a4f787af81ec3682e8ab412b1ed078aee98a19b
joshzich <zichjd@gmail.com>
2015-05-09 13:12:06 -0500

    added test for mark model

--- app/models/mark.rb
+++ app/models/mark.rb
@@ -1,9 +1,4 @@
 class Mark < ActiveRecord::Base
   belongs_to :walk
-  has_many :w
 
-  def create
-    Mark.create(latitude: 5, longitude: 5)
-    #Need geo-location stuff here!!!!!!!!
-  end
 end
--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
 	let!(:gaber) {User.create!(username: "Gaber", email: 'gaber2@gaber.com', password: 'password')}
-	let!(:scruffy) {Dog.create!(name: 'Scruffy', breed: 'pomeranian', birthday: Time.now, owner: gaber )}
+	let!(:scruffy) {Dog.create!(name: 'Scruffy', breed: 'pomeranian', birthday: Time.now, owner: gaber)}
  	it "creates an instance of a dog" do 
  		expect(scruffy).to be_instance_of(Dog)
  	end
