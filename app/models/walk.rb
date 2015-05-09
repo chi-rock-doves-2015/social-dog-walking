228b11605ce02538c54811f294b022ccb26b0a13
CJ Wilson & Erik <pair+cjwilsonjr.erikdstock@devbootcamp.com>
2015-05-09 11:27:11 -0500

    add somehypothetical methods to user and class

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -7,4 +7,18 @@ class Walk < ActiveRecord::Base
   def duration
     updated_at - created_at
   end
+
+  def complete?
+    #should probably be a boolean field 'complete' defaulting to false
+    #return true if self.complete
+    # self.complete = true if last mark is more than 2 hours old
+    # return self.complete
+  end
+
+  def valid?
+    # return whether the game followed our rules
+    # (no impossible walks, obvious motor vehicle travel, etc)
+  end
+
+
 end
