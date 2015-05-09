ec4a8c051675abc04e44b61c5cca797b9f371be0
Chris Bohl <chrisrbohl@gmail.com>
2015-05-09 18:17:58 -0500

    More changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,7 @@ class User < ActiveRecord::Base
   :s3_credentials => {
     :access_key_id => "AKIAIR7ATUOM3FV5GDDQ",
     :secret_access_key => "agLNHSMUFhQLABLMhoAi3SKkdGZKFg8HaF0RFzmw"
-  },
-  :url => ":s3_alias_url", # These two are only required when you alias S3 - e.g. want to use cdn.example.com rather than s3.amazonaws.com
-  :s3_host_alias => "my.aws.alias"
+  }
 
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
