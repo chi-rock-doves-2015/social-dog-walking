cece4e94276b176fc49e16fc3ae330fd5082382f
Chris Bohl <chrisrbohl@gmail.com>
2015-05-09 17:58:49 -0500

    Added s3 info in model based on stack overflow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :avatar,
+  :styles => { :small => '60x60#', :large => '300x300#' }, :default_style => :large,
+  :storage => :s3,
+  :default_url => '/images/:attachment/missing_:style.png',
+  :path => "users/:id/avatar/:style.:extension",
+  :bucket => "bucket name",
+  :s3_credentials => {
+    :access_key_id => "AKIAIR7ATUOM3FV5GDDQ",
+    :secret_access_key => "agLNHSMUFhQLABLMhoAi3SKkdGZKFg8HaF0RFzmw"
+  },
+  :url => ":s3_alias_url", # These two are only required when you alias S3 - e.g. want to use cdn.example.com rather than s3.amazonaws.com
+  :s3_host_alias => "my.aws.alias"
+
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
