bb0b921c1702e544b59535c520b913bcac867a16
Erik Stockmeier <erik.d.stock@gmail.com>
2015-05-09 00:43:48 -0500

    fix session auth - needed :user_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:id] = @user.id
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       @errors = @user.errors.full_messages
