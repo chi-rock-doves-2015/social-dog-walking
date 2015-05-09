0dbab6be5418707002455d8ea2a01c501ea1c4e1
Chris Bohl <chrisrbohl@gmail.com>
2015-05-08 19:42:09 -0500

    Have images working for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
--- db/migrate/20150508193622_avatar_additions.rb
+++
@@ -1,6 +0,0 @@
-class PaperclipExample < ActiveRecord::Migration
-  def change
-    add_attachment :dogs, :avatar
-    add_attachment :user, :avatar
-  end
-end
