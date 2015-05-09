b155ae77026943f26f64b358ec2e57cb189dd648
joshzich <zichjd@gmail.com>
2015-05-09 13:23:51 -0500

    added test for marks active record relation

--- spec/models/mark_spec.rb
+++ spec/models/mark_spec.rb
@@ -1,6 +1,20 @@
 require "rails_helper"
 
-RSpec.describe User, type: :model do
+RSpec.describe Walk, type: :model do
+
+  let!(:gaber) {User.create!(username: "Gaber", email: 'gaber2@gaber.com', password: 'password')}
+  let!(:walk_one) {Walk.create!(user: gaber)}
+  let!(:mark_one) {Mark.create!(latitude: 40.6892, longitude: 74.0444, accuracy: 30, walk: walk_one)}
+  it 'is an instance of walk' do
+    expect(walk_one).to be_instance_of(Walk)
+  end
+
+  it 'has an active record relation called marks with all marks that are associated with it' do 
+  	expect(walk_one.marks.last).to eq(mark_one)
+  end
+end
+
+RSpec.describe Mark, type: :model do
 
   let!(:gaber) {User.create!(username: "Gaber", email: 'gaber2@gaber.com', password: 'password')}
   let!(:walk_one) {Walk.create!(user: gaber)}
