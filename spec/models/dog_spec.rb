d27045845c2cb7688d164abe0d912509424df4e3
joshzich <zichjd@gmail.com>
2015-05-09 13:01:33 -0500

    added test for dog model

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let!(:gaber) {User.create!(username: "Gaber", email: 'gaber2@gaber.com', password: 'password')}
+	let!(:scruffy) {Dog.create!(name: 'Scruffy', breed: 'pomeranian', birthday: Time.now, owner: gaber )}
+ 	it "creates an instance of a dog" do 
+ 		expect(scruffy).to be_instance_of(Dog)
+ 	end
 end
