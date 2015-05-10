99adb02cbe78f4cb203aa45fe73c74ef0a78df2c
joshzich <zichjd@gmail.com>
2015-05-10 10:54:34 -0500

    basic login feature testing done

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -4,15 +4,35 @@ feature "User visits login page" do
   # before do
   #     allow_any_instance_of(Game).to receive(:computer_throw).and_return("scissors")
   # end
+  let!(:user) {User.create!(username: "user", email: 'user@user.com', password: 'password')}
+
   scenario "upon page load user sees login header" do
     visit '/login'
+
+
     expect(page).to have_content "Log in"
   end
 
+
+
   scenario "empy fields will return an error" do 
     visit '/login'
-    click_button('')
+    login_form = page.all('form#login_form')
+    within login_form[0] do 
+      click_button('')
+    end
     expect(page).to have_content "Invalid email or password"
+  end
+
+  scenario "can login with valid email and password" do 
+    visit '/login'
+    login_form = page.all('form#login_form')
+    fill_in 'Password', with: 'password'
+    fill_in 'Email', with: 'user@user.com'
+    within login_form[0] do 
+      click_button('')
+    end
+    expect(page).to have_content "Hello"
   end
 
 end
