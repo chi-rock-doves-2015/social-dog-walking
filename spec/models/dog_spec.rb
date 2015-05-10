97552efb11b79859fd8947993408c74dfacb5e40
joshzich <zichjd@gmail.com>
2015-05-09 21:45:22 -0500

    added tests for dogs

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:josh) {User.create!(username: "josh", email: 'josh@josh.com', password: 'password')}
+  let!(:scruffy) {Dog.create!(name: 'Scruffy', breed: 'Cocker Spaniel', birthday: "July 10, 2006", owner: josh)}
+	it 'is an instance of dog' do 
+		expect(scruffy).to be_instance_of(Dog)
+	end
+
+	it 'has an association called owner' do  
+		expect(scruffy.owner).to eq(josh)
+	end
 end
--- spec/models/walk_spec.rb
+++ spec/models/walk_spec.rb
@@ -1,16 +1,16 @@
 require "rails_helper"
 
-RSpec.describe Mark, type: :model do
+
+RSpec.describe Walk, type: :model do
 
   let!(:josh) {User.create!(username: "josh", email: 'josh@josh.com', password: 'password')}
   let!(:walk_one) {Walk.create!(user: josh)}
   let!(:mark_one) {Mark.create!(latitude: 40.6892, longitude: 74.0444, accuracy: 30, walk: walk_one)}
   it 'is an instance of mark' do
-    expect(mark_one).to be_instance_of(Mark)
+    expect(walk_one).to be_instance_of(Walk)
   end
 
-  it 'has an association called walk' do 
-  	expect(mark_one.walk).to eq(walk_one)
+  it 'has an association called marks' do 
+  	expect(walk_one.marks.last).to eq(mark_one)
   end
 end
-
