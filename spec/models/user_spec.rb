06aa4b80846b7c07be0ae6b7121b5cb9b703ac03
joshzich <zichjd@gmail.com>
2015-05-09 22:44:10 -0500

    added testing for avatar validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
 
+
   let!(:josh) {User.create!(username: "josh", email: 'josh@josh.com', password: 'password')}
+  let(:mortimer) {User.new(username: 'Mortimer', email: 'jim@jim.com', password: 'password', avatar_file_name: 'no such file', avatar_content_type: 'strings', avatar_file_size: '0px')}
   let(:travis) {User.new(username: "Travis", email: 'josh@josh.com', password: 'password')}
   let!(:scruffy) {Dog.create!(name: 'Scruffy', breed: 'Cocker Spaniel', birthday: "July 10, 2006", owner: josh)}
   let!(:sparky) {Dog.create!(name: 'Sparky', breed: 'Great Dane', birthday: "June 9, 2005", owner: josh)}
@@ -30,5 +32,8 @@ RSpec.describe User, type: :model do
   	expect(travis.errors.messages[:email][0]).to eq("has already been taken")
   end
 
-
+  it 'has an error message of avatar invalid if wrong type of file is used' do 
+  	mortimer.save
+  	expect(mortimer.errors.messages[:avatar][0]).to eq("is invalid")
+  end	
 end
