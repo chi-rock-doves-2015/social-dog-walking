371d350d2ff69b13b2bf919374b71928f67b5850
joshzich <zichjd@gmail.com>
2015-05-09 22:35:37 -0500

    added test for validating user email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,33 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
 
-  let!(:gaber) {User.create!(username: "Gaber", email: 'gaber2@gaber.com', password: 'password')}
+  let!(:josh) {User.create!(username: "josh", email: 'josh@josh.com', password: 'password')}
+  let(:travis) {User.new(username: "Travis", email: 'josh@josh.com', password: 'password')}
+  let!(:scruffy) {Dog.create!(name: 'Scruffy', breed: 'Cocker Spaniel', birthday: "July 10, 2006", owner: josh)}
+  let!(:sparky) {Dog.create!(name: 'Sparky', breed: 'Great Dane', birthday: "June 9, 2005", owner: josh)}
+  let!(:walk_one) {Walk.create!(user: josh)}
+  let!(:mark_one) {Mark.create!(latitude: 40.6892, longitude: 74.0444, accuracy: 30, walk: walk_one)}
+
   it 'is a user' do
-    expect(gaber).to be_instance_of(User)
+    expect(josh).to be_instance_of(User)
   end
+
+  it 'has an association of dogs' do 
+  	expect(josh.dogs.count).to eq(2)
+  end
+
+  it 'has an association of walks' do 
+  	expect(josh.walks.last).to be_instance_of(Walk)
+  end
+
+  it 'has an association of marks' do 
+  	expect(josh.marks.last).to be_instance_of(Mark)
+  end
+
+  it 'has an email field that must be unique' do
+  	travis.save
+  	expect(travis.errors.messages[:email][0]).to eq("has already been taken")
+  end
+
+
 end
