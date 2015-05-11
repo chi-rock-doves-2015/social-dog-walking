6ae6e7b081b7f48ac03215afe88435789f343b78
tmpbtz <powers.timothy@gmail.com>
2015-05-11 16:52:23 -0500

    working now

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -34,7 +34,7 @@ class WalksController < ApplicationController
 
     if @walk
       if request.xhr?
-        geojson = MarksHelper.geojson(@walk, "Polygon")
+        geojson = MarksHelper.geojson(@walk, "Point")
         render json: geojson
       else
         render "show"
