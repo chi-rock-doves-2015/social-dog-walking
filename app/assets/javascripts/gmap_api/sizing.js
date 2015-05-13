bcaca71c9c412885b5c3076e6e990d7eaf9123e9
Christopher Bohl <chrisrbohl@gmail.com>
2015-05-12 22:13:50 -0500

    this doesnt work.

--- app/assets/javascripts/gmap_api/sizing.js
+++ app/assets/javascripts/gmap_api/sizing.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  var $windowWidth = document.documentElement.clientWidth;
+  var $windowHeight = document.documentElement.clientHeight;
+  var $topBarHeight = $('.top-bar').height();
+  var $bodyHeight = $windowHeight - $topBarHeight;
   setMapCanvasMobileHeight();
 });
 
@@ -8,8 +12,25 @@ $(document).ready(function() {
 //there has been talk of a sticky/fixed footer button bar
 //that would be positioned above any map for a full-screen floating buttons effect.
 function setMapCanvasMobileHeight(){
-  var $width = document.documentElement.clientWidth;
-  var $height = document.documentElement.clientHeight;
-  // console.log($width);
-  $('#map-canvas').css("height", ($width));
-};
+  console.log('poot');
+  debugger;
+  console.log($bodyHeight);
+  setStatsHeight();
+  setButtonsHeight();
+  $('#map-canvas').css("height", $bodyHeight * 0.45);
+}
+
+function setStatsHeight(){
+  console.log('staffs');
+  $('#stats').css("height", $bodyHeight * 0.3);
+}
+
+
+function setButtonsHeight(){
+  console.log("bootoons");
+  $('#walk-buttons').css({
+    "height" : $bodyHeight * 0.15,
+    "position": "fixed",
+    "bottom": 0
+  });
+}
