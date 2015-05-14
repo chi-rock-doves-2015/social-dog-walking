c56d58712e9434a22a1293f60a71228787ee875d
Chris Bohl <chrisrbohl@gmail.com>
2015-05-14 09:58:01 -0500

    trying with local area

--- app/controllers/local_areas_controller.rb
+++ app/controllers/local_areas_controller.rb
@@ -1,9 +1,9 @@
 class LocalAreasController < ApplicationController
   def show
-    local_area = LocalArea.new(params[:latitude], params[:longitude])
+    @local_area = LocalArea.new(params[:latitude], params[:longitude])
 
-    @local_area_users = local_area.users
-    puts "LOCALAREAUSERS"
-    puts @local_area_users
+    # @local_area_users = local_area.users
+    # puts "LOCALAREAUSERS"
+    # puts @local_area_users
   end
 end
