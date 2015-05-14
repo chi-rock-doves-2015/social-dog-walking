cc4bcb3d8f83d7a173446f308e64afeceffb855e
Chris Bohl <chrisrbohl@gmail.com>
2015-05-13 19:36:41 -0500

    Changing branches for different problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      session[:user_id] = @user.id
       redirect_to "/users/#{@user.id}/welcome"
     else
       @errors = @user.errors.full_messages
