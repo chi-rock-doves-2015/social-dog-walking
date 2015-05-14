a068f1955c8e4411e57a9ebefc4290cb67c6ad56
Chris Bohl <chrisrbohl@gmail.com>
2015-05-14 09:20:19 -0500

    heroku test

--- app/controllers/local_areas_controller.rb
+++ app/controllers/local_areas_controller.rb
@@ -3,5 +3,7 @@ class LocalAreasController < ApplicationController
     local_area = LocalArea.new(params[:latitude], params[:longitude])
 
     @local_area_users = local_area.users
+    puts LOCALAREAUSERS
+    puts @local_area_users
   end
 end
