a28bc9e9dc00c9597ec6812010c675d3fbd6d9e8
Chris Bohl <chrisrbohl@gmail.com>
2015-05-13 19:06:11 -0500

    fixing new user welcome problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,6 +60,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def new_user_welcome
+    @user = User.find_by(id: session[:id])
+  end
+
   private
   def user_params
      params.require(:user).permit(:username, :email, :avatar, :password)
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,7 +2,7 @@ class Dog < ActiveRecord::Base
 
   has_attached_file :avatar,
   :styles => { :thumb => '60x60#', :medium => '200x200#', :large => '300x300#' }, :default_style => :large,
-  :default_url => '/images/:attachment/missing_:style.png',
+  :default_url => '/images/:attachment/dog-avatar.png',
   :path => "users/:id/avatar/:style.:extension"
 
   belongs_to  :owner, class_name: "User"
