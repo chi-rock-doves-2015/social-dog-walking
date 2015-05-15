54233d1ad7ebc357e00ed64efd4c14a4b179d2b7
Erik <erik.d.stock@gmail.com>
2015-05-14 20:45:49 -0500

    update method to subtract a user from their hood to take id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
     @user = User.find_by(id: session[:user_id])
     ##hacky- use location later
     mark = @user.marks.last
-    @local_area = LocalArea.new(mark.latitude, mark.longitude, current_user)
+    @local_area = LocalArea.new(mark.latitude, mark.longitude, current_user.id)
 
     if session[:user_id]
       render 'dashboard'
--- app/services/local_area.rb
+++ app/services/local_area.rb
@@ -1,7 +1,7 @@
 class LocalArea
   attr_reader :marks, :neighbors, :dogs
 
-  def initialize(lat, lon, current_user)
+  def initialize(lat, lon, current_user_id = current_user.id)
     point = "'POINT " + "(" + lat.to_s + " " + lon.to_s + ")" + "'"
 
     sql = "SELECT * FROM marks WHERE ST_DWithin(coords, #{point}, 1000);"
@@ -16,13 +16,20 @@ class LocalArea
       mark.walk.user
     end.uniq.compact
 
-
     @dogs = @marks.map do |mark|
       mark.walk.dogs
     end.flatten.compact.uniq
 
-    @dogs -= current_user.dogs
-    @neighbors -= [current_user]
+    subtract_current_user(current_user_id)
 
   end
+
+
+  private
+
+  def subtract_current_user(current_user_id)
+    @dogs -= Dog.where(owner_id: current_user_id)
+    @neighbors -= [User.find_by(id: current_user_id)]
+  end
+
 end
