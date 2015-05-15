a2d8291a705a1797eb5720af2f06cc128584ddd4
tmpbtz <powers.timothy@gmail.com>
2015-05-14 22:49:39 -0500

    this is working and good to go

--- app/controllers/local_areas_controller.rb
+++ app/controllers/local_areas_controller.rb
@@ -1,8 +1,8 @@
 class LocalAreasController < ApplicationController
   def create
     if request.xhr?
-      @local_area = LocalArea.new(params["latitude"], params["longitude"])
-      geojson = LocalAreasHelper.geojson(@local_area.users, "Polygon")
+      @local_area = LocalArea.new(params["latitude"], params["longitude"], current_user.id)
+      geojson = TerritoriesHelper.geojson(current_user, @local_area.neighbors, "Polygon")
       render json: geojson
     else
       render render :nothing => true, status: 404
